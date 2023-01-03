3ce616eabce6e641d64b2fb9b2f3d3d39a7d9389
Mur <murmavilink@gmail.com>
2023-01-03 17:20:01 +0600

    рефакторинг modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,19 +1,39 @@
 const modal = () => {
     const popupBtns = document.querySelectorAll('.popup-btn');
     const popup = document.querySelector('.popup');
+    const popupContent = document.querySelector('.popup-content');
     const popupClose = popup.querySelector('.popup-close');
 
+    let count = 0;
+    let idRequest;
+
+    const screenCheck = () => {
+        return document.documentElement.clientWidth < 768 ? true : false;
+    };
+
+    const modalAnim = () => {
+        count++;
+
+        idRequest = requestAnimationFrame(modalAnim);
+        popupContent.style.top = count + '%';
+
+        if(count > 30) cancelAnimationFrame(idRequest);
+    };
 
     popupBtns.forEach(popupBtn => {
         popupBtn.addEventListener('click', () => {
             popup.style.display = 'block';
+
+            !screenCheck() ? idRequest = requestAnimationFrame(modalAnim) : '';
+            
         });
     });
     
     popupClose.addEventListener('click', () => {
+        count = 0;
+        popupContent.style.top = 10 + '%';
         popup.style.display = 'none';
     });
-
 
 };
 
