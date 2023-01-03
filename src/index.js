f1187f13f21c3338e097a341335318ff55e63457
Mur <murmavilink@gmail.com>
2023-01-03 19:46:32 +0600

    функционал smoothScroll сделано

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
-
+import smoothScroll from './modules/smoothScroll';
 
 timer('31 december 2023');
 menu();
 modal();
+smoothScroll();
