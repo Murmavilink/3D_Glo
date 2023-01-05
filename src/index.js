58e28b08b4cb7ce6af26d25439d66b0355c3c075
Mur <murmavilink@gmail.com>
2023-01-05 22:03:38 +0600

    функционал tabs.js сделано, рефакторинг modal.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import smoothScroll from './modules/smoothScroll';
+import tabs from './modules/tabs';
 
 timer('31 december 2023');
 menu();
 modal();
 smoothScroll();
+tabs();
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,7 +2,6 @@ const modal = () => {
     const popupBtns = document.querySelectorAll('.popup-btn');
     const popup = document.querySelector('.popup');
     const popupContent = document.querySelector('.popup-content');
-    const popupClose = popup.querySelector('.popup-close');
 
     let count = 0;
     let idRequest;
@@ -23,16 +22,16 @@ const modal = () => {
     popupBtns.forEach(popupBtn => {
         popupBtn.addEventListener('click', () => {
             popup.style.display = 'block';
-
             !screenCheck() ? idRequest = requestAnimationFrame(modalAnim) : '';
-            
         });
     });
-    
-    popupClose.addEventListener('click', () => {
-        count = 0;
-        popupContent.style.top = 10 + '%';
-        popup.style.display = 'none';
+
+    popup.addEventListener('click', (e) => {
+        if(!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+            count = 0;
+            popupContent.style.top = 10 + '%';
+            popup.style.display = 'none';
+        }
     });
 
 };
