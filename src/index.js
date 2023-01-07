f676c9df3b1be88e1f3e7a63e8b24da7b9e6c6c0
Mur <murmavilink@gmail.com>
2023-01-07 17:12:13 +0600

    функционал validate сделано

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import modal from './modules/modal';
 import smoothScroll from './modules/smoothScroll';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
-
+import validate from './modules/validate';
 
 timer('31 december 2023');
 menu();
@@ -12,3 +12,4 @@ modal();
 smoothScroll();
 tabs();
 slider('portfolio-content', 'portfolio-item', 'portfolio-dots', 'portfolio-item-active', 'dot-active');
+validate();
---
+++ src/modules/validate.js
@@ -0,0 +1,70 @@
+const validate = () => {
+    const calcBlock = document.querySelector('.calc-block');
+    const calcSquareInput = calcBlock.querySelector('.calc-square');
+    const calcCountInput = calcBlock.querySelector('.calc-count');
+    const calcDayInput = calcBlock.querySelector('.calc-day');
+    const calcInputs = [calcSquareInput, calcCountInput, calcDayInput];
+
+    const nameInputs = document.querySelectorAll('.form-name');
+
+    const mailInputs = document.querySelectorAll('.form-email');
+    
+    const telInputs = document.querySelectorAll('.form-phone');
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+    };
+
+    calcInputs.forEach(input => {
+        input.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/\D/, "");
+        });
+    });
+
+    nameInputs.forEach(input => {
+        input.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^а-яё -]/i, "");
+        });
+    });
+
+    nameInputs.forEach(input => {
+        input.addEventListener('blur', (e) => {
+            const result = [];
+            let nameList;
+            e.target.value = e.target.value.replace(/^[\s\-]+|[\s\-]+$/g, '')
+                .replace(/\s{2,}/g, ' ').replace(/\-{2,}/g, '-');
+
+            nameList = e.target.value.match(/[а-яё\-]+/gi);
+            if (nameList) {
+                nameList.forEach(name => {
+                    if (/\-+/.test(name)) {
+                        let res = [];
+                        let subNames = name.match(/[а-яё]+/gi);
+                        subNames.forEach(n => res.push(capitalizeFirstLetter(n)));
+                        result.push(res.join('-'));
+                    } else {
+                        result.push(capitalizeFirstLetter(name));
+                    }
+                });
+            }
+
+            e.target.value = result.join(' ');
+        });
+    });
+
+    mailInputs.forEach(input => {
+        input.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^a-z@\-_.!~*']/i, "");
+        });
+    });
+
+    telInputs.forEach(input => {
+        input.addEventListener('input', (e) => {
+            e.target.value = e.target.value.replace(/[^\d\(\)\-]/, "");
+        });
+    });
+
+
+};
+
+export default validate;
